a91cb0917c0fb2bf00605423283b2464ce6ac5f0
HtppsPratik <pratik.thapa.1223@gmail.com>
2024-01-30 14:57:51 +0545

    updated

--- accounts/manager.py
+++ accounts/manager.py
@@ -36,5 +36,3 @@ class UserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError(('super must have is_superuser=True.'))
         return self.create_user(email, password, **extra_fields)
-
-        
